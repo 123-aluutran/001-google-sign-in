bb061c6997664ec0f7c70d01a3416a664177ce8a
An Luu <atluutran@gmail.com>
2025-02-03 15:30:56 -0500

    Added GoogleSignIn package and used it

--- google-sign-in/google-sign-in/google_sign_inApp.swift
+++ google-sign-in/google-sign-in/google_sign_inApp.swift
@@ -6,14 +6,34 @@
 //
 
 import SwiftUI
-import SwiftData
+
+import GoogleSignIn
 
 @main
-struct google_sign_inApp: App {
-    let SRC = "google_sign_in"
+struct googleSigInApp: App {
+    let SRC = "googleSigInApp"
     var body: some Scene {
         WindowGroup {
             Text(SRC)
+                .onAppear {
+                    GIDSignIn.sharedInstance.restorePreviousSignIn { user, error in
+                        if user != nil {
+                            print("\(SRC): user = \(user!)|")
+                        }
+                        else {
+                            print("\(SRC): ERROR: User is nil")
+                        }
+                        
+                        if error != nil {
+                            print("\(SRC): ERROR = \(error!)|")
+                        }                    }
+                }
+                .onOpenURL { url in
+                    let SRC = self.SRC + ".onOpenURL"
+                    print("\(SRC): url = \(url)|")
+                    
+                    GIDSignIn.sharedInstance.handle(url)
+                }
         }
     }
 }

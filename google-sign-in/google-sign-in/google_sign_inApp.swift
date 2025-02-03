3b738882d31decc2b6d5ca54ff4a8a217331d5a6
An Luu <atluutran@gmail.com>
2025-02-03 15:04:19 -0500

    Created: Placeholder app

---
+++ google-sign-in/google-sign-in/google_sign_inApp.swift
@@ -0,0 +1,19 @@
+//
+//  google_sign_inApp.swift
+//  google-sign-in
+//
+//  Created by An Luu on 2/3/25.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct google_sign_inApp: App {
+    let SRC = "google_sign_in"
+    var body: some Scene {
+        WindowGroup {
+            Text(SRC)
+        }
+    }
+}
---
+++ google-sign-in/google-sign-inTests/google_sign_inTests.swift
@@ -0,0 +1,17 @@
+//
+//  google_sign_inTests.swift
+//  google-sign-inTests
+//
+//  Created by An Luu on 2/3/25.
+//
+
+import Testing
+@testable import google_sign_in
+
+struct google_sign_inTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
